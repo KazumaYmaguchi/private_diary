dc485e0353113adf0c1ede540d91edbdc7cfeb26
KazumaYamaguchi <ksw2070048@stu.o-hara.ac.jp>
2021-09-11 08:41:00 +0000

    メッセージ

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,7 @@
 # from re import template
 from django.shortcuts import render
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import InquiryForm
@@ -14,3 +16,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "diary/inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
+        return super().form_valid(form)
